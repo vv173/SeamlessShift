b1a11476e38bd112f76475167e0b1e0b3143b3b9
wojcikkuba <wojcikjakub@int.pl>
2023-09-20 21:53:34 +0200

    Remove elements

--- src/pages/DashboardPages/NftDashboardPage.tsx
+++ src/pages/DashboardPages/NftDashboardPage.tsx
@@ -2,13 +2,10 @@ import React from 'react';
 import { PageTitle } from '@app/components/common/PageTitle/PageTitle';
 import { References } from '@app/components/common/References/References';
 import { useResponsive } from '@app/hooks/useResponsive';
-import { TrendingCreators } from '@app/components/nft-dashboard/trending-creators/TrendingCreators';
-import { RecentlyAddedNft } from '@app/components/nft-dashboard/recently-added/RecentlyAddedNft';
 import { TrendingCollections } from '@app/components/nft-dashboard/trending-collections/TrendingCollections';
 import { Balance } from '@app/components/nft-dashboard/Balance/Balance';
 import { TotalEarning } from '@app/components/nft-dashboard/totalEarning/TotalEarning';
 import { ActivityStory } from '@app/components/nft-dashboard/activityStory/ActivityStory';
-import { RecentActivity } from '@app/components/nft-dashboard/recentActivity/RecentActivity';
 import * as S from './DashboardPage.styles';
 import { BaseRow } from '@app/components/common/BaseRow/BaseRow';
 import { BaseCol } from '@app/components/common/BaseCol/BaseCol';
@@ -21,22 +18,13 @@ const MedicalDashboardPage: React.FC = () => {
       <S.LeftSideCol xl={16} xxl={17} id="desktop-content">
         <BaseRow gutter={[60, 60]}>
           <BaseCol span={24}>
-            <TrendingCreators />
-          </BaseCol>
-
-          <BaseCol span={24}>
-            <RecentlyAddedNft />
-          </BaseCol>
-
-          <BaseCol span={24}>
             <TrendingCollections />
           </BaseCol>
 
-          <BaseCol span={24}>
-            <RecentActivity />
-          </BaseCol>
         </BaseRow>
+
         <References />
+        
       </S.LeftSideCol>
 
       <S.RightSideCol xl={8} xxl={7}>
@@ -57,22 +45,9 @@ const MedicalDashboardPage: React.FC = () => {
 
   const mobileAndTabletLayout = (
     <BaseRow gutter={[20, 24]}>
-      <BaseCol span={24}>
-        <TrendingCreators />
-      </BaseCol>
-
-    {/*
-      <BaseCol span={24}>
-        <RecentlyAddedNft />
-      </BaseCol>
-  */}
-
+     
       <BaseCol span={24}>
         <TrendingCollections />
-      </BaseCol>
-
-      <BaseCol span={24}>
-        <RecentActivity />
       </BaseCol>
     </BaseRow>
   );
