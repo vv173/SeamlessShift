d24b5fda22e8d7ee8cee52d84f3881bd776ee51f
wojcikkuba <wojcikjakub@int.pl>
2023-10-31 19:49:46 +0100

    Change of names

--- src/components/header/components/profileDropdown/ProfileOverlay/ProfileOverlay.tsx
+++ src/components/header/components/profileDropdown/ProfileOverlay/ProfileOverlay.tsx
@@ -9,7 +9,7 @@ export const ProfileOverlay: React.FC = ({ ...props }) => {
   return (
     <div {...props}>
       <S.Text>
-        <Link to="/profile">{t('profile.title')}</Link>
+        <Link to="/my-account">{t('profile.title')}</Link>
       </S.Text>
       <S.ItemsDivider />
       <S.Text>
